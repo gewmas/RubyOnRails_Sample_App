14836fb468624ab45c75ce3ec5eb1cfa79038d18
Yuhua Mai <yuhua.mai@duke.edu>
2013-11-25 14:05:54 -0500

    Finish CH6 Exercise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ActiveRecord::Base
-	before_save { self.email = email.downcase }
+	has_secure_password
+	before_save { email.downcase! }
 	validates :name, presence: true, length: { maximum: 50 }
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	# VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	# A valid email regex that disallows double dots in email addressed. 
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 	validates :email, presence:   true,
 	format:     { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 	has_secure_password
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -62,36 +62,47 @@ describe User do
 		it { should_not be_valid }
 	end
 
-	describe "when password is not present" do
-		before do
-			@user = User.new(name: "Example User", email: "user@example.com", password: " ", password_confirmation: " ")
-		end
-		it { should_not be_valid }
-	end
+	describe "email address with mixed case" do
+		let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
 
-	describe "when password doesn't match confirmation" do
-		before { @user.password_confirmation = "mismatch" }
-		it { should_not be_valid }
-	end
-
-	describe "with a password that's too short" do
-		before { @user.password = @user.password_confirmation = "a" * 5 }
-		it { should be_invalid }
-	end
-
-	describe "return value of authenticate method" do
-		before { @user.save }
-		let(:found_user) { User.find_by(email: @user.email) }
-
-		describe "with valid password" do
-			it { should eq found_user.authenticate(@user.password) }
-		end
-
-		describe "with invalid password" do
-			let(:user_for_invalid_password) { found_user.authenticate("invalid") }
-
-			it { should_not eq user_for_invalid_password }
-			specify { expect(user_for_invalid_password).to be_false }
+		it "should be saved as all lower-case" do
+			@user.email = mixed_case_email
+			@user.save
+			expect(@user.reload.email).to eq mixed_case_email.downcase
 		end
 	end
+	
+# Password
+describe "when password is not present" do
+	before do
+		@user = User.new(name: "Example User", email: "user@example.com", password: " ", password_confirmation: " ")
+	end
+	it { should_not be_valid }
+end
+
+describe "when password doesn't match confirmation" do
+	before { @user.password_confirmation = "mismatch" }
+	it { should_not be_valid }
+end
+
+describe "with a password that's too short" do
+	before { @user.password = @user.password_confirmation = "a" * 5 }
+	it { should be_invalid }
+end
+
+describe "return value of authenticate method" do
+	before { @user.save }
+	let(:found_user) { User.find_by(email: @user.email) }
+
+	describe "with valid password" do
+		it { should eq found_user.authenticate(@user.password) }
+	end
+
+	describe "with invalid password" do
+		let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+
+		it { should_not eq user_for_invalid_password }
+		specify { expect(user_for_invalid_password).to be_false }
+	end
+end
 end
