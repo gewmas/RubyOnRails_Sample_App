21cab0ed466d1bbad29bb92b16945fbbcf0168e9
Yuhua Mai <yuhua.mai@duke.edu>
2013-11-24 20:49:59 -0500

    Add helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,11 +4,17 @@ describe "Static pages" do
 
 	subject { page }
 
+	shared_examples_for "all static pages" do
+		it { should have_selector('h1', text: heading) }
+		it { should have_title(full_title(page_title)) }
+	end
+
 	describe "Home page" do
 		before { visit root_path }
+		let(:heading)    { 'Sample App' }
+		let(:page_title) { '' }
 
-		it { should have_content('Sample App') }
-		it { should have_title(full_title('')) }
+		it_should_behave_like "all static pages"
 		it { should_not have_title('| Home') }
 	end
 
@@ -29,7 +35,23 @@ describe "Static pages" do
 	describe "Contact page" do
 		before { visit contact_path }
 
-		it { should have_content('Contact') }
+		it { should have_selector('h1', text: 'Contact') }
 		it { should have_title(full_title('Contact')) }
+	end
+
+	it "should have the right links on the layout" do
+		visit root_path
+		click_link "About"
+		expect(page).to have_title(full_title('About Us'))
+		click_link "Help"
+		expect(page).to have_title(full_title('Help'))
+		click_link "Contact"
+		expect(page).to have_title(full_title('Contact'))
+
+		click_link "Home"
+		click_link "Sign up now!"
+		expect(page).to have_title(full_title('Sign up'))
+		click_link "sample app"
+		expect(page).to have_title(full_title(''))
 	end
 end
