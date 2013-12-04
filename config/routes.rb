5be93af9299fb2909ca474cd67c4f95b86e72648
myhgew <yuhua.mai@duke.edu>
2013-12-04 16:02:20 -0500

    Implement till 11.2.3 Following and followers pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 SampleApp::Application.routes.draw do
-  resources :users
+  resources :users do
+    member do
+      # /users/1/following, /users/1/followers
+      get :following, :followers
+    end
+  end
+
   resources :sessions, only: [:new, :create, :destroy]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 
   root 'static_pages#home'
   match '/signup',  to: 'users#new',            via: 'get'
