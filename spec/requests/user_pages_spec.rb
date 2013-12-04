9f548a785dd5b23e8c589c431d582204e116b6e1
myhgew <yuhua.mai@duke.edu>
2013-12-03 19:41:03 -0500

    Finish All Users

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -99,4 +99,22 @@ describe "User pages" do
 			specify { expect(user.reload.email).to eq new_email }
 		end
 	end
+
+	describe "index" do
+		before do
+			sign_in FactoryGirl.create(:user)
+			FactoryGirl.create(:user, name: "Bob", email: "bob@example.com")
+			FactoryGirl.create(:user, name: "Ben", email: "ben@example.com")
+			visit users_path
+		end
+
+		it { should have_title('All users') }
+		it { should have_content('All users') }
+
+		it "should list each user" do
+			User.all.each do |user|
+				expect(page).to have_selector('li', text: user.name)
+			end
+		end
+	end
 end
