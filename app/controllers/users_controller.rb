40a3248ebd9f071cde2bbc8cf36c622787fda49e
myhgew <yuhua.mai@duke.edu>
2013-12-04 16:18:22 -0500

    Implement Following and followers pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-	before_action :signed_in_user, only: [:index, :edit, :update]
+	before_action :signed_in_user, only: [:index, :edit, :update, :destroy, :following, :followers]
 	before_action :correct_user,   only: [:edit, :update]
 	before_action :admin_user,     only: :destroy
 
@@ -49,7 +49,19 @@ class UsersController < ApplicationController
 		redirect_to users_url
 	end
 
+	def following
+		@title = "Following"
+		@user = User.find(params[:id])
+		@users = @user.followed_users.paginate(page: params[:page])
+		render 'show_follow'
+	end
 
+	def followers
+		@title = "Followers"
+		@user = User.find(params[:id])
+		@users = @user.followers.paginate(page: params[:page])
+		render 'show_follow'
+	end
 
 
 	private
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -108,11 +108,11 @@ describe "User pages" do
 			specify { expect(user.reload.email).to eq new_email }
 		end
 
-		# By issuing a PATCH request directly to the update method as shown in Listing 9.48, 
-		# verify that the admin attribute isn’t editable through the web. 
-		# Be sure to get first to Red, and then to Green. 
-		# (Hint: Your first step should be to add admin to the list of permitted parameters 
-		# in user_params.)
+	# By issuing a PATCH request directly to the update method as shown in Listing 9.48, 
+	# verify that the admin attribute isn’t editable through the web. 
+	# Be sure to get first to Red, and then to Green. 
+	# (Hint: Your first step should be to add admin to the list of permitted parameters 
+	# in user_params.)
 describe "forbidden attributes" do
 	let(:params) do
 		{ user: { admin: true, password: user.password,
@@ -186,6 +186,34 @@ describe "index" do
 				end
 				it { should_not have_link('delete', href: user_path(admin)) }
 			end
+		end
+	end
+
+	describe "following/followers" do
+		let(:user) { FactoryGirl.create(:user) }
+		let(:other_user) { FactoryGirl.create(:user) }
+		before { user.follow!(other_user) }
+
+		describe "followed users" do
+			before do
+				sign_in user
+				visit following_user_path(user)
+			end
+
+			it { should have_title(full_title('Following')) }
+			it { should have_selector('h3', text: 'Following') }
+			it { should have_link(other_user.name, href: user_path(other_user)) }
+		end
+
+		describe "followers" do
+			before do
+				sign_in other_user
+				visit followers_user_path(other_user)
+			end
+
+			it { should have_title(full_title('Followers')) }
+			it { should have_selector('h3', text: 'Followers') }
+			it { should have_link(user.name, href: user_path(user)) }
 		end
 	end
 end
