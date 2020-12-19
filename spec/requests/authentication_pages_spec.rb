8bdebce40981ff81b04fdcde864ff50f0e8a0be7
Yuhua Mai <yuhua.mai@gmail.com>
2020-12-19 11:07:05 -0800

    Fix rspec visit path

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -4,7 +4,7 @@ describe 'Authentication' do
   subject { page }
 
   describe 'signin page' do
-    before { visit '/signin' }
+    before { visit signin_path }
 
     it { should have_content('Sign in') }
     it { should have_title('Sign in') }
